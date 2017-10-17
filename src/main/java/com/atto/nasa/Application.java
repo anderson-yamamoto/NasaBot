bfb3fae73d41c5a0606b71e8878b590c921f93c0
Anderson Yamamoto <attogba@gmail.com>
2017-10-17 17:38:13 -0200

    Spring Boot basics

---
+++ src/main/java/com/atto/nasa/Application.java
@@ -0,0 +1,12 @@
+package com.atto.nasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
