a4f0dec2bcb4a3e51bfcfad53d68bf1da210916f
Anderson Yamamoto <attogba@gmail.com>
2017-10-18 23:00:01 -0200

    Updated to String format for the sake of readability

--- src/main/java/com/atto/nasa/util/SimpleTerrainFormatter.java
+++ src/main/java/com/atto/nasa/util/SimpleTerrainFormatter.java
@@ -8,10 +8,7 @@ import com.atto.nasa.domain.Terrain;
 @Service
 public class SimpleTerrainFormatter implements TerrainFormatter {
 
-	private static final String SEPARATOR = ",";
-
 	public String format(Terrain t, RobotOrientation i) {
-		return new StringBuilder(10).append("(").append(t.getPosX()).append(SEPARATOR).append(t.getPosY())
-				.append(SEPARATOR).append(i.name()).append(")").toString();
+		return String.format("(%d,%d,%s)", t.getPosX(), t.getPosY(),i);
 	}
 }
